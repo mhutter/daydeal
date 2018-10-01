b7dc319c43c61762d44f84fa700d05be98019aa9
Manuel Hutter <manuel.hutter@vshn.ch>
2018-10-01 15:35:08 +0200

    Update selectors

--- main.go
+++ main.go
@@ -14,16 +14,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	title := doc.Find("h1.meta-first-line").First().Text()
-	subtitle := doc.Find("span.meta-second-line").First().Text()
+	title := doc.Find(".product-description__title1").First().Text()
+	subtitle := doc.Find(".product-description__title2").First().Text()
 
-	price := doc.Find("span.price").First().Text()
+	price := doc.Find(".product-pricing__prices-new-price").First().Text()
 	originalPrice := doc.Find("div.originalPrice span").First().Text()
 	originalPrice = strings.TrimSpace(originalPrice)
 	originalPrice = strings.TrimSuffix(originalPrice, "*")
 	originalPrice = strings.TrimSpace(originalPrice)
 
-	percentage := doc.Find("span.percentage").First().Text()
+	percentage := doc.Find(".product-progress__availability").First().Text()
 
 	fmt.Printf("\n    %s\n    %s\n\n", title, subtitle)
 	fmt.Printf("Für CHF %s anstatt %s\n", price, originalPrice)
