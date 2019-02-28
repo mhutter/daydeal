f60159ef643203e0f062b32b4e87f95e44e89977
Daniel Steudler <daniel.steudler@gmx.ch>
2019-02-28 14:04:40 +0100

    Add next output for when next deal is

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -23,6 +24,16 @@ var (
 func init() {
 	flag.BoolVar(&fetchDOTW, "w", fetchDOTW, "Fetch the deal of the Week instead")
 	flag.Parse()
+}
+
+func fmtDuration(d time.Duration) string {
+	d = d.Round(time.Second)
+	h := d / time.Hour
+	d -= h * time.Hour
+	m := d / time.Minute
+	d -= m * time.Minute
+	s := d / time.Second
+	return fmt.Sprintf("%02d:%02d:%02d", h, m, s)
 }
 
 func main() {
@@ -54,7 +65,17 @@ func run(out io.Writer, url string) {
 
 	percentage := doc.Find(".product-progress__availability").First().Text()
 
+	// the website returns the moment of the next deal like "2006-01-02 15:04:05"
+	// in UTC without announcingt that it is UTC.
+	nextDealUTC := doc.Find("span.js-clock").AttrOr("data-next-deal", "")
+	nextDeal, _ := time.ParseInLocation("2006-01-02 15:04:05", nextDealUTC, time.Local)
+
+	nextDealIn := time.Until(nextDeal)
+	nextDealInFmt := fmtDuration(nextDealIn)
+
 	fmt.Fprintf(out, "\n    %s\n    %s\n\n", title, subtitle)
 	fmt.Fprintf(out, "Für %s %s (%s)\n", price, originalPrice, priceSource)
 	fmt.Fprintf(out, "Noch %s verfügbar\n", percentage)
+	// Golang time formatting: https://flaviocopes.com/go-date-time-format/
+	fmt.Fprintf(out, "Nächster Deal am: %s (in %s)\n", nextDeal.Format("Mon Jan _2 15:04:05"), nextDealInFmt)
 }
